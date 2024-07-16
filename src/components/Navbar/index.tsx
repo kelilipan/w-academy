9d3f81733b100748efccf33f782d5efafd0ad58a
Wisesa <hi@wisesa.dev>
2024-07-16 23:06:25 +0700

    chore: fix linter not working

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -39,7 +39,7 @@ const Navbar = () => {
                 <ul className="grid gap-3 p-6 md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr]">
                   <li className="row-span-3">
                     <NavigationMenuLink asChild>
-                      <a
+                      <Link
                         className="flex h-full w-full select-none flex-col justify-end rounded-md bg-gradient-to-b from-muted/50 to-muted p-6 no-underline outline-none focus:shadow-md"
                         href="/"
                       >
@@ -50,7 +50,7 @@ const Navbar = () => {
                           Lorem ipsum dolor sit amet consectetur adipisicing
                           elit.
                         </p>
-                      </a>
+                      </Link>
                     </NavigationMenuLink>
                   </li>
                   {/* @todo: using internal router */}
